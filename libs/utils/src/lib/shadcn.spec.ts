6a59806c4e77a67a38f76c44ab78b39220c141eb
bobopotato <cshong1999@hotmail.com>
2024-12-18 00:47:07 +0800

    chore: revise test cases

--- libs/utils/src/lib/shadcn.spec.ts
+++ libs/utils/src/lib/shadcn.spec.ts
@@ -1,7 +1,7 @@
-// import { utils } from './utils';
+import { cn } from "./shadcn.util";
 
-// describe('utils', () => {
-//   it('should work', () => {
-//     expect(utils()).toEqual('utils');
-//   });
-// });
+describe("utils", () => {
+  it("should work", () => {
+    expect(cn("bg-red-50", "bg-black")).toEqual("bg-black");
+  });
+});
