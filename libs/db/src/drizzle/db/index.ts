36c3aa9bfc045f79301ec0c6f9488177d77842a3
bobopotato <cshong1999@hotmail.com>
2025-01-21 18:16:36 +0800

    chore: test

--- libs/db/src/drizzle/db/index.ts
+++ libs/db/src/drizzle/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 export const createDatabase = (databaseUrl: string) => {
+  console.log("databaseUrl", databaseUrl);
   const pool = new Pool({
     connectionString: databaseUrl
   });
