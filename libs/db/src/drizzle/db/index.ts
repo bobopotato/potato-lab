94f28d6a6dbc9e931a2bbf3998ac93243fb85cdc
bobopotato <cshong1999@hotmail.com>
2025-01-21 18:25:17 +0800

    test

--- libs/db/src/drizzle/db/index.ts
+++ libs/db/src/drizzle/db/index.ts
@@ -3,6 +3,7 @@ import { Pool } from "pg";
 
 export const createDatabase = (databaseUrl: string) => {
   console.log("databaseUrl", databaseUrl);
+  console.log("env", process.env);
   const pool = new Pool({
     connectionString: databaseUrl
   });
